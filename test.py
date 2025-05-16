ca7f4526af1753e144a25c4f322f2be29b19b730
Wilfred Apollon <dev@achila.me>
2025-05-16 14:20:08 -0500

    product refinement

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 
 import main 
+import mimetypes
 import inspect 
 import pyunit 
 import hypothesis
 
 def test_base_map():
-    from main import db
+    from main import Base
+    signature=list(inspect.signature(cls).parameter)
+    class DecodeBase:
+        pass
+    class DecodeLocal:
+    def __isinstance__()
+        
     ...
