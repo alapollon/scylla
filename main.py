03663b5686a13b17f5683d35d1327d000c2145a2
Wilfred Apollon <dev@achila.me>
2025-05-30 09:58:56 -0500

    product refinement

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm.decl_api import DeclarativeMeta
 from pandas import DataFrame, MultiIndex, Series
 import ssl 
 import time
-import numpy as npy 
+import numpy as np
 import threading
 import netfilterqueqe
 import queue
@@ -35,7 +35,7 @@ PATH = (
     Keys= '~/.ssh/*',
     Log= '/var/log/scylla/*')
 
-class SystemsProgramming:
+class OperatingSystemProgramming:
     def __init__(self, file_data ):
         self.add_file_body= file 
         with PATH as those_paths
@@ -57,18 +57,19 @@ class SystemsProgramming:
     def __slot__():
         pass
 
-event=threading.Event()
-que=netfilterqueqe.NetfilterQueqe()
-local_server_local_address= 
+osi=OperatingSystemProgramming
+channel=netfilterqueqe.NetfilterQueqe()
 sql=sqlite3
-register=atexit.register()
-array=npy.array()
+tex=atexit.register()
+narray=np.array
 df=DataFrame
 series=Series
 mi= MultiIndex
 
 from ss import StreamRequestHandler, DatagramRequestHandler
-class DatabaseServer(StreamRequestHandler, DatagramRequestHandler):
+link_stream=StreamRequestHandler
+link_datagram=DatagramRequestHandler
+class DatabaseServer():
     def __init__(self, time, publickey, secure_socket_layer_certificate, *args):
         local, port=args 
         self.origin=(local,port)
@@ -76,9 +77,6 @@ class DatabaseServer(StreamRequestHandler, DatagramRequestHandler):
         self.timedate=
         self.keys={}
         pass 
-    def _inbound_queue(): 
-        pass
-
     @staticmethod
     def handlr(self,load,time=self.timedate):
         def timecount():
@@ -93,9 +91,14 @@ class DatabaseServer(StreamRequestHandler, DatagramRequestHandler):
             pass
         
 db=DatabaseServer
+data_pipeline_event=threading.Event(
+
+)
 async def database_handle(func):
-    db.handlr()
-    pass
+     server=db.handlr()
+     while func is not None:
+        await server
+    return pass ...
 
 handlr_thread=thread.threadding(
     name="outbound data pipeline ",
@@ -126,7 +129,6 @@ class FrameworkData(IndividualFactorDistribution, MultiFactorDistribution):
     def __init__(self, stack):
         super().__init__()
         pass 
-from typing import Optionals
 from sqlalchemy import (
 create_engine,
 Select,
@@ -154,7 +156,9 @@ Mapped,
 relationship,
 alliased,
 bundle)
-from sqlalchemy.engine import create_engine, URL as url 
+from sqlalchemy.engine import URL as url 
+from typing import Optional
+api=url
 class Base(declarative_base):
     type_annotation_map={
         int:BigIntger()
@@ -166,210 +170,201 @@ class Base(declarative_base):
         edges:LargeBinary(),
         fitness:Boolean(),
     }
-    @declared_attr
     def __init__(self):
-        self.api=url
         self.engine= create_engine( echo=True)
         self.orm= sessionmaker(bind=self.engine)
         self.session=Session
         self.meta= MetaData(bind=self.engine)
         self.imperative_mapper=registry(self.meta=self.meta)
-
-    class Main_Gateway_Scheme:
-        __tablename__= "main_gateway__scheme"
-        mac_uuid: Mapped[String["Primary_Table_Schema"]]=relationship(back_populates="uuid")
-        mac: Mapped[String["Primary_Table_Schema"]]=relationship(back_populates="mac")
-        gatewayipv6=mapped_column("gatway6",String(), PrimaryKey=True, unique= True, nullable=False) 
-        cidr=mapped_column("gateway", Integer(),PrimaryKey=True, nullable=False)
-        gatewayipv4=Column("gateway4",String(), nullable=True)
-        hops=Column("hops", Integer(), nullable=False)
-        gatewayname=Column("dns",String(), primary_key=False , nullable= True )
-        domain=Column("Company", Binary(), unique=True, nullable=False )
-        def __init__(self,):
-            cidr, gateway4, gateway6, hops, 
-            self.cidr=cidr
-            self.hops=hops 
-            self.gateway4=gateway4|None 
-            self.gateway6=gateway6|None
-    class Port_Service_Schemes: 
-        __tablename__="port_services"
-        services=mapped_column()
-        oem_ports=mapped_column()
-        true=
-        false=
-    class Node_Edge_Scheme:
-        __tablename__= "node_edge_scheme"
-        mapid=Column("id", Integer(), unique=True, nullable=False)
-        uuid: Mapped[]= relationship(back_populates=)
-        mac: Mapped[String[]]=relationship(back_populates="")
-        gateway: Mapped[]=relationship(back_populates=)
-        edges: Mapped[LargeBinary[]]=mapped_column()
-        host_count=mapped_column("hosts", BigIntger(), nullable=True, ForeignKey=True)
-        nodes_count=mapped_column("nodes", BigIntger(), nullable=True)
-        ifgateway=Column("isgateway",Boolean(), primary_key=True, nullable=False)
-        bgp=Column()
-    class Port_Services_Relationship_Scheme:
-        __tablename__= "service_map_relationship"
-        uuid: Mapped []=mapped_column("uuid",Binary(), primary_key=True)
-        gateway=Column("gateway",Binary(16),PrimaryKey=True)
-        node=mapped_column("node", Boolean(),PrimaryKey)
-        targetipv6=Column("targetipv6",primary_key=True, nullable=False)
-        services=Column("servicesList", LargeBinary(), nullable=False)
-        edges: Mapped[[]]=relationship
-        def __init__(self,*args):
-            array, node, 
-            self.service_map=array
-            self.nodeif=node 
-            self.ed
-    class Kansas_Cinncinati__Schema:
-        __tablename__="kansas_cincinnati__scheme"
-        uuid: Mapped[]=Column("uuid",unique=True,primary_key=True,nullable=True)
-        hops=Column(Binary(), nullable=False)
-        hostmac=Column(nullable=True)
-        hostipv6=Column()
-        gatecid=Column("cidr",Binary(), nullable=False  )
-        gateway6=Column()
-        headgateway6: Mapped[]=mapped_column(Primary_key=True, nullable=False )
-        bgp=Column("bgp",Boolean(),)
-        edges=Column("edges",Array(), nullable=False )
-        port=("map",Array(), nullable=False)
-        def __init__(self, *args)
-            targetipv6, mac, hostname=args
-            self.targetipv6=targetipv6
-            self.hostname=hostname
-            self.mac=mac 
-    class Route_Table_Schema:
-        __tablename__="route_schemes"
-        mapid=Column()
-        target: Mapped[[]]=mapped_column()
-        gateway6: Mapped[]=mapped_column( Binary(), primary_key=True, nullable=False)
-        edges: Mapped[]=mapped_column(LargeBinary(), nullable=False )
-        bgp=Column()
-        hops=Column("hops", Binary(), primary_key=True, nullable=False)
-        def __init__(self, edges: list, ):
-            self.edges=edges
-        def _hash_host_over_edges():
+        pass
+@
+class Main_Gateway_Scheme:
+    __tablename__= "main_gateway__scheme"
+    mac_uuid: Mapped[String["Primary_Table_Schema"]]=relationship(back_populates="uuid")
+    mac: Mapped[String["Primary_Table_Schema"]]=relationship(back_populates="mac")
+    gatewayipv6=mapped_column("gatway6",String(), PrimaryKey=True, unique= True, nullable=False) 
+    cidr=mapped_column("gateway", Integer(),PrimaryKey=True, nullable=False)
+    gatewayipv4=Column("gateway4",String(), nullable=True)
+    hops=Column("hops", Integer(), nullable=False)
+    gatewayname=Column("dns",String(), primary_key=False , nullable= True )
+    domain=Column("Company", Binary(), unique=True, nullable=False )
+    def __init__(self,):
+        cidr, gateway4, gateway6, hops, 
+        self.cidr=cidr
+        self.hops=hops 
+        self.gateway4=gateway4|None 
+        self.gateway6=gateway6|None
+@
+class Port_Service_Schemes: 
+    __tablename__="port_services"
+    uuid: []=mapped_column()
+    application_ports: Mapped[]=mapped_column()
+    oem_ports: Mapped[]=mapped_column()
+    def __init__(self):
+        pass
+@
+class Node_Edge_Scheme:
+    __tablename__= "node_edge_scheme"
+    mapid=Column("id", Integer(), unique=True, nullable=False)
+    uuid: Mapped[]= relationship(back_populates=)
+    mac: Mapped[String[]]=relationship(back_populates="")
+    gateway: Mapped[]=relationship(back_populates=)
+    edges: Mapped[LargeBinary[]]=mapped_column()
+    host_count=mapped_column("hosts", BigIntger(), nullable=True, ForeignKey=True)
+    nodes_count=mapped_column("nodes", BigIntger(), nullable=True)
+    ifgateway=Column("isgateway",Boolean(), primary_key=True, nullable=False)
+    bgp=Column()
+    def __init__():
+        mac, edges, gateway=
+        self.
+@
+class Port_Services_Relationship_Scheme:
+    __tablename__= "service_map_relationship"
+    uuid: Mapped []=mapped_column("uuid",Binary(), primary_key=True)
+    gateway=Column("gateway",Binary(16),PrimaryKey=True)
+    node=mapped_column("node", Boolean(),PrimaryKey)
+    targetipv6=Column("targetipv6",primary_key=True, nullable=False)
+    services=Column("servicesList", LargeBinary(), nullable=False)
+    edges: Mapped[[]]=relationship
+    def __init__(self,*args):
+        array, node=args
+        self.service_map=array
+        self.nodeif=node 
+        self.e
+@
+class Kansas_Cinncinati__Schema:
+    __tablename__="kansas_cincinnati__scheme"
+    uuid: Mapped[]=Column("uuid",unique=True,primary_key=True,nullable=True)
+    hops=Column(Binary(), nullable=False)
+    hostmac=Column(nullable=True)
+    hostipv6=Column()
+    gatecid=Column("cidr",Binary(), nullable=False  )
+    gateway6=Column()
+    headgateway6: Mapped[]=mapped_column(Primary_key=True, nullable=False )
+    bgp=Column("bgp",Boolean(),)
+    edges=Column("edges",Array(), nullable=False )
+    port=("map",Array(), nullable=False)
+    def __init__(self, *args)
+        targetipv6, mac, hostname=args
+        self.targetipv6=targetipv6
+        self.hostname=hostname
+        self.mac=mac 
+@
+class Route_Table_Schema:
+    __tablename__="route_schemes"
+    mapid=Column()
+    target: Mapped[[]]=mapped_column()
+    gateway6: Mapped[]=mapped_column( Binary(), primary_key=True, nullable=False)
+    edges: Mapped[]=mapped_column(LargeBinary(), nullable=False )
+    bgp=Column()
+    hops=Column("hops", Binary(), primary_key=True, nullable=False)
+    def _hash_host_over_edges():
+        pass
+    def __init__(self, edges: list, ):
+        self.edges=edges
+    
+@
+class Database_Table_Schema:
+    __tablename__="database_routes_schema"
+    uuid=relationship()
+    host6: Mapped[Opti]=Column("ipv6", Binary(), )
+    sub=Column()
+    hostname=Column("url", Binary(), nullable=False)
+    url=mapped_column("url", Binary(), nullable=False)
+    fitness=Column("fitness",Boolean(), nullable=False)
+    masks=Column()
+    def __init__(self, host, url, **kwargs):
+        self.host=host
+        self.sub=
+        self.hostname=kwargs.get
+        self.url=url
+        self.fitness=kwargs.get
+        self.masks= 
+@
+class Primary_Table_Schema:
+    __tablename__ = "mac_table__schema"
+    mapid=Column()
+    uuid=mapped_column("uuid", String(),PrimaryKey=True, unique=True, nullable=False)
+    hops=Column("Hops", Integer(), )
+    mac: Mapped[Optional[str]]=mapped_column( PrimaryKey=True, unique=True, )
+    latency=Column()
+    zone: Mapped[Optional[]]=mapped_column(PrimaryKey=True, unique=False, nullable=True)
+    cidr=mapped_column("cidr",Integer(), nullable=False)
+    update: Mapped[]=Column()     
+    def __init__(self, **kwargs):
+        self.mac=
+        self.cidr=cidr
+        self.latency=latency
+        self.zone=zone
+        self.update=update
+async def get(self, issue: str, **kwargs):
+        work=FrameworkData
+        _order_stmt=select(kwargs.get).order_by(kwargs.get)
+        _joint_stmt=select(kwargs.get['']).join(kwargs.get['']).order_by(kwargs.get[''],kwargs.get[''])
+        _bundle_stmt=select(
+            Bundle(kwargs.get['']),
+            Bundle(kwargs.get[''])
+        ).join_from(kwargs.get[''])
+            def query(stat)->:
+            if meta:
+                return enumerate.session.execute(stat)
+        pull=query       
+        if issue == ".joint":
+            pull(_joint_stmt)
             pass
-    class Database_Table_Schema:
-        __tablename__="database_routes_schema"
-        uuid=relationship()
-        host6: Mapped[Opti]=Column("ipv6", Binary(), )
-        sub=Column()
-        hostname=Column("url", Binary(), nullable=False)
-        url=mapped_column("url", Binary(), nullable=False)
-        fitness=Column("fitness",Boolean(), nullable=False)
-        masks=Column()
-        def __init__(self, host, url, **kwargs):
-            self.host=host
-            self.sub=
-            self.hostname=kwargs.get
-            self.url=url
-            self.fitness=kwargs.get
-            self.masks= 
-    class Primary_Table_Schema:
-        __tablename__ = "mac_table__schema"
-        mapid=Column()
-        uuid=mapped_column("uuid", String(),PrimaryKey=True, unique=True, nullable=False)
-        hops=Column("Hops", Integer(), )
-        mac: Mapped[Optional[str]]=mapped_column( PrimaryKey=True, unique=True, )
-        latency=Column()
-        zone: Mapped[Optional[]]=mapped_column(PrimaryKey=True, unique=False, nullable=True)
-        cidr=mapped_column("cidr",Integer(), nullable=False)
-        update: Mapped[]=Column()     
-        def __init__(self, **kwargs):
-            self.mac=
-            self.cidr=cidr
-            self.latency=latency
-            self.zone=zone
-            self.update=update
-    gateway_table=alliased(Main_Gateway_Scheme(),name="gatewaySchmes")
-    node_table=alliased(Node_Edge_Scheme(),name="nodeSchmes") 
-    port_table=alliased(Port_Services_Relationship(),name="portSchmes")
-    target_table=alliased(Kansas_Cinncinati__Schema(),name="kcSchmes")
-    route_table=alliased(Route_Table_Schema(),name="routeSchmes")
-    database_table=alliased(Database_Table_Schema(),name="dbSchmes")
-    mac_table=alliased(Primary_Table_Schema(),name="macSchmes")
-       
-   async def get(self, issue: str, **kwargs):
-            work=FrameworkData
-            _order_stmt=select(kwargs.get).order_by(kwargs.get)
-            _joint_stmt=select(kwargs.get['']).join(kwargs.get['']).order_by(kwargs.get[''],kwargs.get[''])
-            _bundle_stmt=select(
-                Bundle(kwargs.get['']),
-                Bundle(kwargs.get[''])
-            ).join_from(kwargs.get[''])
-             def query(stat)->:
-                if meta:
-                    return enumerate.session.execute(stat)
-            pull=query       
-            if issue == ".joint":
-                pull(_joint_stmt)
-                return df
-            elif issue==".joint": 
-                query(_bundle_stmt):
-                return
-            elif issue=".joint":
-                query(_order_stmt).all():
-            
-   def _init_databse(self, *args):
-        driver, 
-        orm=self.orm
-        session=self.session
-        meta=self.meta
-        engine=self.engine(self.api.set() echo=True)
-        def _init_sqlite():
-            while engine:
-                try: 
-                    session.get_transaction()
-                except ...:
+        elif issue==".joint": 
+            pass
+        elif issue=".joint":
+            pass   
+def _init_databse(self, *args):
+    ...
+    def _raise_sqlite_tables():
+        while engine:
+            try: 
+                session.get_transaction()
+            except ...:
+                session.rollback()
+    def _init_postgre_database():
+         imperative_mapper.map_imperatively() 
+        while engine:
+            try:
+                meta.create_all(engine)
+            except ...: 
+                if err: 
                     session.rollback()
-        def _init_postgres():
-            imperative_mapper.map_imperatively(
-
-            )
-            while engine:
-                try:
-                    meta.create_all(engine)
-                except ...: 
-                    if err: 
-                        session.rollback()
-                    pass 
-                pass
-
-    class Private: 
-        @classmethod
-        def __init__(self, *args: )-> Self:
-            self.token= args.get
+                pass 
             pass
+    if engine:
+        _init_postgre_database()
+    else:
+        sql_link=api.set()
+        Base.meta.create_engine(link )
+
+class Private: 
+    @classmethod
+    def __init__(self, *args: )-> Self:
+        self.token= args.get
+        pass
 
 class LocalDatabase(Base):
     def __init__(self, **kwargs)
-        if self.meta...: 
-        else: 
-             super().__init__(**kwargs):
-      def _class_basename():
-        val= self.drive=drivename +"_"+self.host=host 
+       super().__init__()
         pass
-
-
-    
 class RemoteDatabase(Base):
     def __init__(self, **kwargs):
          super().__init__(**kwargs)
-        
-    def _class_basename():
-        val= self.drive=drivename +"_"+self.host=host 
-        pass
-    
-class Userdatabase(thread.threadding, type):
+         pass 
+    def __repr__():
+       pass
+class Userdatabase(type):
     _registry= { }
     items=t.__bases__()
     def __init_subclass(cls, , **kwargs: string):
         namespace=type.__prepare__(cls, ())
         args= list(inspect.signature(cls).parameters)
         signature=','.join('{self.%s!r}'% arg for arg in args)
-        code='def __call__(cls, , ) -> type:\n'
-        code+=f'  return super().__new__(,,,,)'
+        code='def __new__( ) -> type:\n'
+        code+=f'  return super().__init__()'
         header={}
         exec(
             code,
@@ -378,27 +373,17 @@ class Userdatabase(thread.threadding, type):
    def __get__(meta, clsname, bases, methods):
         namespace[f'{}']= 
         return 
-    @classmethod
-    def factor():
-        pass
-
-    async def _search_():
+  
+metaclass=Userdatabase
+class ProductDatabase(thread.threadding):
+    def __init__(remote: Boolean, *args, **kwargs):
+        self.api=
+        self.stack=[]
         pass 
-      
-class ProductDatabase( metaclass=Userdatbase):
-    def __init__(remote: Boolean, **kwargs):
-        if !remote:
-            pass 
-        elif remote :
-            pass
-        else: 
-           pass 
-   
-
-
+    search= 
+    commit=
 product = ProductDatabase()
 index = FrameworkData()
-
 def user_input_target(self, **kwargs):
     if kwargs.get == ['target*']:
             target.append(kwargs.get['target*']) 
@@ -425,6 +410,16 @@ def scan_prompt_data(self) -> :
     else :
         sys.stdout.write()
 
+class Nmap_Scanner_Server(thread.threadding):
+    def __init__(self,):
+       dst, option,  
+       self.hosts=[ i for in ]
+       self.nmap_channel=channel.bind
+       pass 
+    @classmethod
+    def _delete_dst_property():
+        pass
+
 def whois(parameter:string,broadcast:string):
     async def _discover_host_on_edge():
         from cap import ARP as arp
@@ -439,9 +434,7 @@ def whois(parameter:string,broadcast:string):
         run=asyncio.run()
         while is not !run:
             index=series(,index=["destination","origin","lan"])
-            for 
             
-
 def link():
     ...
 def spoof(packet):
@@ -461,10 +454,6 @@ def spoof(packet):
                 del packet[cap.UDP].chksum
                 del packet[cap.UDP].len 
             packet.set_payload(str(cap_packet))
-
-
-
-
 scan_input_thread = threading.Thread(
     name=' scanning user input %()s'
     target = scan_prompt_data,
@@ -486,12 +475,7 @@ def main():
   parser.addd_arguement('get', action=, default=False)
   parser.add_arguemnt('--spoof',action=,default=False)
   parser.add_arguement('--connect',action=,default=False)
-  while :
-    if :
-        queue = q.bind(0, )
-    elif :
-    
-    elif :
+
 
 if __name__ is __main__:
     main()
